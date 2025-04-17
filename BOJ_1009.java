2b69252290a8377a40b5ff1cd132f65bb326c9f6
seoyeon2001 <syeon1250@naver.com>
2025-04-17 17:43:39 +0900

    solved: baekjoon_1009 - 분산처리

---
+++ BOJ_1009.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1009 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < T; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            int result = 1;
+            for(int j = 1; j <= b; j++) {
+                result = result * a % 10;
+            }
+
+            if (result == 0) {
+                System.out.println(10);
+            } else {
+                System.out.println(result);
+            }
+        }
+    }
+}
