2e33b0ec30c7c40457c013e6d21e9666a2d10648
seoyeon2001 <syeon1250@naver.com>
2025-04-28 11:34:54 +0900

    solved: baekjoon_1541 - 잃어버린 괄호

---
+++ BOJ_1541.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1541 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputs = br.readLine().split("-");
+//        System.out.println(Arrays.toString(inputs));
+
+        int[] result = new int[inputs.length];
+        for(int i = 0; i < inputs.length; i++) {
+
+            if (inputs[i].contains("+")) {
+                StringTokenizer st = new StringTokenizer(inputs[i], "+"); // \\+
+
+                int tmp = 0;
+                while(st.hasMoreTokens()) {
+                    tmp += Integer.parseInt(st.nextToken());
+                }
+
+                result[i] = tmp;
+            } else {
+                result[i] = Integer.parseInt(inputs[i]);
+            }
+        }
+
+        int answer = result[0];
+        for (int i = 1; i < result.length; i++) {
+            answer -= result[i];
+        }
+
+        System.out.println(answer);
+    }
+}
