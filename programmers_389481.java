41d933fecb0c598283d2dddd006bccc31585c0d4
seoyeon2001 <113911282+seoyeon2001@users.noreply.github.com>
2025-10-01 22:05:29 +0900

    solved: programmers_389481 - 봉인된 주문

---
+++ programmers_389481.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+class programmers_389481 {
+    public String solution(long n, String[] bans) {        
+        long[] nums = new long[bans.length];
+        for(int j = 0; j < bans.length; j++) {
+            long num = 0;
+            int wordSize = bans[j].length();
+            
+            for(int i = 0; i < wordSize; i++) {                
+                num += (bans[j].charAt(i) - 'a' + 1) * (Math.pow(26, wordSize - i - 1));
+            }            
+            nums[j] = num;
+        }
+        Arrays.sort(nums);
+        
+        int cnt = 0;
+        for(long num : nums) {
+            if(num <= n + cnt) {
+                cnt++;
+            } else break;       
+        }
+        n += cnt;
+        
+        int len = 1;
+        long count = 26, base = 0;
+        while (n > base + count) {
+            base += count;
+            count *= 26;
+            len++;
+        }
+        
+        long offset = n - base - 1;
+        
+        char[] word = new char[len];
+        for (int i = len - 1; i >= 0; i--) {
+            word[i] = (char)('a' + (offset % 26));
+            offset /= 26;
+        }
+        
+        return new String(word);
+    }
+}
