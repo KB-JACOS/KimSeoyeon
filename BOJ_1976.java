37efebec4565302774d1dcb1c1d167225236c9ec
seoyeon2001 <syeon1250@naver.com>
2025-10-02 13:31:42 +0900

    solved: baekjoon_1976 - 여행 가자

---
+++ BOJ_1976.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1976 {
+
+    static int[] parent;
+
+    static int N;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        N = Integer.parseInt(br.readLine()); // 도시 수
+        int M = Integer.parseInt(br.readLine()); // 여행 계획 도시 수
+
+        parent = new int[N + 1];
+        for (int i = 1; i <= N; i++) {
+            parent[i] = i;
+        }
+
+        for (int i = 1; i <= N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= N; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+
+                // 연결된 부분은 합집합 연산
+                if (temp == 1) {
+                    union(i, j);
+                }
+            }
+        }
+
+        st = new StringTokenizer(br.readLine());
+        int start = find(Integer.parseInt(st.nextToken()));
+        for (int i = 1; i < M; i++) {
+            int now = Integer.parseInt(st.nextToken());
+
+            // 맨 처음 출발 도시와 연결되어있지 않은 도시가 있으면 여행 계획이 불가능
+            if (start != find(now)) {
+                System.out.println("NO");
+                return;
+            }
+        }
+
+        System.out.println("YES");
+    }
+
+    // x의 부모를 찾는 연산
+    public static int find(int x) {
+        if (x == parent[x]) {
+            return x;
+        }
+        return parent[x] = find(parent[x]);
+    }
+
+    public static void union(int x, int y) {
+        x = find(x);
+        y = find(y);
+
+        if (x != y) {
+            if (x < y) {
+                parent[y] = x;
+            } else {
+                parent[x] = y;
+            }
+        }
+    }
+}
