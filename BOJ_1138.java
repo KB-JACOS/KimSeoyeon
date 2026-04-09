71df537b75f2f173b655326837922cdded8f8db3
seoyeon2001 <syeon1250@naver.com>
2026-04-09 15:24:14 +0900

    solved: BOJ_1138 - 한 줄로 서기

---
+++ BOJ_1138.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class BOJ_1138 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        int[] ans = new int[n];
+
+        st = new StringTokenizer(br.readLine());
+        for(int i = 1; i <= n; i++) {
+            int idx = Integer.parseInt(st.nextToken());
+
+            int time = 0;
+            for(int j = 0; j < n; j++) {
+                if(ans[j] == 0) {
+                    if(idx == time) {
+                        ans[j] = i;
+                        break;
+                    }
+                    time++;
+                }
+            }
+        }
+
+        for(int i : ans) {
+            System.out.print(i + " ");
+        }
+    }
+}
